ae028b1fd53e93971ce09b20e21955647cf95c7f
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2021-07-19 19:17:19 +0300

    Add ListStarship component
    I added the ListStarship component and cleaned the Home component.

---
+++ src/components/ListStarships.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="starships">
+    <div v-for="ship in starships" :key="ship.id">
+      <router-link :to="'/' + ship.url.split('/').reverse()[1]">
+        <div class="shipCard">
+          <img src="../assets/starship.png" width="100" />
+          <div class="shipCard-info">
+            <h3>{{ ship.name }}</h3>
+            <p>{{ ship.model }}</p>
+            <p>{{ ship.hyperdrive_rating }}</p>
+          </div>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'ListStarships',
+  data() {
+    return {
+      search: '',
+      starships: [],
+    }
+  },
+  methods: {
+    handleSearch: function() {
+      axios
+        .get(`https://swapi.dev/api/starships?search=${this.search}`)
+        .then((res) => {
+          this.starships = [...res.data.results]
+        })
+    },
+  },
+  mounted() {
+    axios.get(`https://swapi.dev/api/starships`).then((res) => {
+      this.starships = [...res.data.results]
+    })
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,19 @@
 <template>
   <div class="home">
     <HelloWorld />
-    <div class="starships">
-      <div v-for="ship in starships" :key="ship.id">
-        <router-link :to="'/' + ship.url.split('/').reverse()[1]">
-          <div class="shipCard">
-            <img src="../assets/starship.png" width="100" />
-            <div class="shipCard-info">
-              <h3>{{ ship.name }}</h3>
-              <p>{{ ship.model }}</p>
-              <p>{{ ship.hyperdrive_rating }}</p>
-            </div>
-          </div>
-        </router-link>
-      </div>
-    </div>
+    <ListStarships />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 import HelloWorld from '../components/HelloWorld'
+import ListStarships from '../components/ListStarships'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
-  },
-  data() {
-    return {
-      search: '',
-      starships: [],
-    }
-  },
-  methods: {
-    handleSearch: function() {
-      axios
-        .get(`https://swapi.dev/api/starships?search=${this.search}`)
-        .then((res) => {
-          this.starships = [...res.data.results]
-        })
-    },
-  },
-  mounted() {
-    axios.get(`https://swapi.dev/api/starships`).then((res) => {
-      this.starships = [...res.data.results]
-    })
+    ListStarships,
   },
 }
 </script>
