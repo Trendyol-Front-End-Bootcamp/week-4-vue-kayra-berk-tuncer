60d13cce65581969bfc9b3164505f1205259ed9b
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2021-07-19 18:47:04 +0300

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '../views/Home'
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+  mode: 'history',
+  linkActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+  ],
+})
