f17f77e1a4aea3c66e1cec3db77c382187156b06
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2021-07-21 12:26:46 +0300

    Add search bar

--- src/components/ListStarships.vue
+++ src/components/ListStarships.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="starships">
-    <div v-for="ship in starships" :key="ship.id">
-      <router-link :to="'/' + ship.url.split('/').reverse()[1]">
-        <div class="shipCard">
-          <img src="../assets/starship.png" width="100" />
-          <div class="shipCard-info">
-            <h3>{{ ship.name }}</h3>
-            <p>{{ ship.model }}</p>
-            <p>{{ ship.hyperdrive_rating }}</p>
+  <div class="home-content">
+    <div class="search-bar">
+      <input type="text" placeholder="Search by name" v-model="search" />
+      <button @click="handleSearch">Search</button>
+    </div>
+    <div class="starships">
+      <div v-for="ship in starships" :key="ship.id">
+        <router-link :to="'/' + ship.url.split('/').reverse()[1]">
+          <div class="shipCard">
+            <img src="../assets/starship.png" width="200" />
+            <div class="shipCard-info">
+              <h3>{{ ship.name }}</h3>
+              <p>{{ ship.model }}</p>
+              <p>{{ ship.hyperdrive_rating }}</p>
+            </div>
           </div>
-        </div>
-      </router-link>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -30,15 +36,33 @@ export default {
     handleSearch: function() {
       axios
         .get(`https://swapi.dev/api/starships?search=${this.search}`)
-        .then((res) => {
-          this.starships = [...res.data.results]
+        .then((response) => {
+          this.starships = [...response.data.results]
         })
     },
   },
   mounted() {
-    axios.get(`https://swapi.dev/api/starships`).then((res) => {
-      this.starships = [...res.data.results]
+    axios.get(`https://swapi.dev/api/starships`).then((response) => {
+      this.starships = [...response.data.results]
     })
   },
 }
 </script>
+
+<style scoped>
+.home-content {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+.starships {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
+}
+.shipCard {
+  width: 400px;
+  border: 1px solid black;
+}
+</style>
